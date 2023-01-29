9b0df17e9e7d6d383f353162a46b4200a2ab261e
suicideDuck22 <rdorneles64@gmail.com>
2023-01-29 11:23:48 -0300

    Added structs examples.

---
+++ struct-defining-and-instantiating/src/main.rs
@@ -0,0 +1,45 @@
+fn main() {
+    //The fields don't need to follow the same order.
+    let mut user1: User = User {
+        active: true,
+        username: String::from("renas");
+        email: String::from("renas@gmail.com");
+        sing_in_count: 1,
+    }
+
+    //Note wich to change the value, the struct instance need to be muttable.
+    user1.email("newrenas@gmail.com");
+
+    //Creating a new User instance using a factory function.
+    let user_created_with_factory: User = user_factory(String::from("renasfact@gmail.com"), String::from("renasfact"));
+
+    let user_with_update_syntax = User {
+        username: String::from("uptrenas");
+        email: String::from("uptrenas@gmail.com");
+        //IMPORTANT!!!
+        //This inform Rust to create the rest of the fields with the same value
+        //contained on the user1 instance.
+        //Because their values are primitive types, it's implement the Copy, instead Move.
+        //But if we do with the types that implement Move, user1 is no longer available to user.
+        ..user1;
+    }
+}
+
+//Structure is formed by fields with name and their type.
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sing_in_count: u64,
+}
+
+fn user_factory(email: String, username: String) -> User {
+    User {
+        active: true,
+        //Because username and email fields have the same name of the parameters.
+        //we don't need to repeat their.
+        username,
+        email,
+        sing_in_count: 1
+    }
+}
---
+++ struct-tuples/src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+    //Like tuples, you can access a value by their name dot index.
+    //And using this, each Struct have is your own type, so a fn who receive a Color, cannot receive a Point.
+    let black = Color(0, 0, 0);
+    let origin = Point(0, 0, 0);
+
+    let subject = AlwaysEqual;
+}
+
+//Here we define the name of the Struct, with their tuple values.
+struct Color(i32, i32, i32);
+struct Point(i32, i32, i32);
+
+//It's a unit-like struct.
+//A struct without any value, most used for traits.
+struct AlwaysEqual;
