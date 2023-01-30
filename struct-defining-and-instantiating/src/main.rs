edc9ca991d9d43526e817ae4393c958057b4ea26
suicideDuck22 <rdorneles64@gmail.com>
2023-01-30 20:11:23 -0300

    Fixed some error on struct-defining

--- struct-defining-and-instantiating/src/main.rs
+++ struct-defining-and-instantiating/src/main.rs
@@ -2,27 +2,30 @@ fn main() {
     //The fields don't need to follow the same order.
     let mut user1: User = User {
         active: true,
-        username: String::from("renas");
-        email: String::from("renas@gmail.com");
+        username: String::from("renas"),
+        email: String::from("renas@gmail.com"),
         sing_in_count: 1,
-    }
+    };
 
     //Note wich to change the value, the struct instance need to be muttable.
-    user1.email("newrenas@gmail.com");
+    user1.email = String::from("newrenas@gmail.com");
 
     //Creating a new User instance using a factory function.
-    let user_created_with_factory: User = user_factory(String::from("renasfact@gmail.com"), String::from("renasfact"));
+    let _user_created_with_factory: User = user_factory(
+        String::from("renasfact@gmail.com"),
+        String::from("renasfact"),
+    );
 
-    let user_with_update_syntax = User {
-        username: String::from("uptrenas");
-        email: String::from("uptrenas@gmail.com");
+    let _user_with_update_syntax = User {
+        username: String::from("uptrenas"),
+        email: String::from("uptrenas@gmail.com"),
         //IMPORTANT!!!
         //This inform Rust to create the rest of the fields with the same value
         //contained on the user1 instance.
         //Because their values are primitive types, it's implement the Copy, instead Move.
         //But if we do with the types that implement Move, user1 is no longer available to user.
-        ..user1;
-    }
+        ..user1
+    };
 }
 
 //Structure is formed by fields with name and their type.
@@ -40,6 +43,6 @@ fn user_factory(email: String, username: String) -> User {
         //we don't need to repeat their.
         username,
         email,
-        sing_in_count: 1
+        sing_in_count: 1,
     }
 }
