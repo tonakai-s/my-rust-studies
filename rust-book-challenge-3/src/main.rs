bef0ba561002a2b12c75cbae416cfb4359531034
suicideDuck22m <rdorneles64@gmail.com>
2023-02-10 20:38:12 -0300

    Finished the core of the challenge 3

--- rust-book-challenge-3/src/main.rs
+++ rust-book-challenge-3/src/main.rs
@@ -19,7 +19,9 @@ fn main() {
             .read_line(&mut solicitation)
             .unwrap();
 
-        match solicitation.as_str().trim() {
+        solicitation = solicitation.trim().to_string();
+
+        match solicitation.as_str() {
             "Add" => add_employee(
                 &mut company_employees,
             ),
@@ -62,6 +64,46 @@ fn add_employee(
 fn list_employees(
     map_of_employees: &HashMap<String, Vec<String>>
 ) {
-    println!("Here's the map of the company:");
-    println!("{:#?}", map_of_employees);
+    println!("Type 'All' to list all company, or type the Department you whant to look: ");
+    let mut list_type = String::new();
+
+    io::stdin()
+        .read_line(&mut list_type)
+        .unwrap();
+    
+    list_type = list_type.trim().to_string();
+
+    match list_type.as_str() {
+        "All" => list_all_company(&map_of_employees),
+        _ => {
+            println!("{:?}", list_type);
+            list_by_department(&list_type, &map_of_employees)
+        },
+    }
+}
+
+fn list_by_department(department: &String, company_map: &HashMap<String, Vec<String>>){
+    let department_employees: Option<&Vec<String>> = company_map.get(department);
+    match department_employees {
+        Some(employees) => {
+            let mut employees = employees.clone();
+            employees.sort();
+
+            println!("The employees of the department {department}:");
+            println!("{}", employees.join("\r\n"));
+        },
+        None => println!("This department not exist!")
+    }
+}
+
+fn list_all_company(company_map: &HashMap<String, Vec<String>>) {
+    println!("Here is the map of the company!");
+    println!("Printing on pattern [Department => Employees]");
+
+    for(key, value) in company_map {
+        let mut employees = value.clone();
+        employees.sort();
+
+        println!("{key} => {}", employees.join(" | "));
+    }
 }
