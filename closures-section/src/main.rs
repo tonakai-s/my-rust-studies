9369b35471d86cb2889ea9a6e4666d47d6952a54
suicideDuck22 <rdorneles64@gmail.com>
2023-03-06 20:36:28 -0300

    Added closure section

---
+++ closures-section/src/main.rs
@@ -0,0 +1,12 @@
+use std::thread;
+
+fn main(){
+    let list = vec![1, 2, 3];
+    println!("Before defining closure: {:?}", list);
+
+    thread::spawn(move || println!("From thread: {:?}", list))
+        .join()
+        .unwrap();
+
+    // println!("After defining closure: {:?}", list);
+}
